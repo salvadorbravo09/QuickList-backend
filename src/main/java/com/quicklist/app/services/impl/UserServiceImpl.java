8990ba6c79befce738b47ea6a5f0b34e5d83bca1
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-08-12 19:12:30 -0400

    creacion del servicio y su implementacion

---
+++ src/main/java/com/quicklist/app/services/impl/UserServiceImpl.java
@@ -0,0 +1,42 @@
+package com.quicklist.app.services.impl;
+
+import com.quicklist.app.entities.User;
+import com.quicklist.app.repositories.UserRepository;
+import com.quicklist.app.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<User> findAll() {
+        return (List<User>) userRepository.findAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    @Transactional
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(Long id) {
+        userRepository.deleteById(id);
+    }
+}
