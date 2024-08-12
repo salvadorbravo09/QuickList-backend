66a0e9c55d019d06226ba8f66bcda7900191262e
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-08-12 18:49:21 -0400

    creacion de arquitectura principal y creacion de entidad principal

---
+++ src/main/java/com/quicklist/app/entities/User.java
@@ -0,0 +1,29 @@
+package com.quicklist.app.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(name = "last_name")
+    private String lastname;
+
+    private String email;
+    private String username;
+    private String password;
+}
