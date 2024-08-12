4194199b5dafd0d59ee101ced4fd8b7d79e40dad
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-08-12 18:56:29 -0400

    creacion de la clase repository

---
+++ src/main/java/com/quicklist/app/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.quicklist.app.repositories;
+
+import com.quicklist.app.entities.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+}
