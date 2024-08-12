0ac0eb4f2f1d95c47b87f508630d5d7f0a72fc1e
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-08-12 18:36:17 -0400

    creacion del proyecto backend

---
+++ src/main/java/com/quicklist/app/SpringbootQuicklistApplication.java
@@ -0,0 +1,13 @@
+package com.quicklist.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootQuicklistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootQuicklistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quicklist/app/SpringbootQuicklistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quicklist.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootQuicklistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
