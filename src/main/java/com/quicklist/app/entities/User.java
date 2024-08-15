8126420576e945d11cc876d5fc601378ce4c105c
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-08-15 14:43:52 -0400

    se agregaron las anotaciones de validacion en la clase entity

--- src/main/java/com/quicklist/app/entities/User.java
+++ src/main/java/com/quicklist/app/entities/User.java
@@ -1,6 +1,9 @@
 package com.quicklist.app.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,12 +21,21 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     private String name;
 
+    @NotBlank
     @Column(name = "last_name")
     private String lastname;
 
+    @NotBlank
+    @Email
     private String email;
+
+    @NotBlank
+    @Size(min = 4, max = 16)
     private String username;
+
+    @NotBlank
     private String password;
 }
