87a6703ec7b8a8f2b06f23e5f4c1aa18197aa5a2
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-08-14 21:04:22 -0400

    se agrego la anotacion para los cors

--- src/main/java/com/quicklist/app/controllers/UserController.java
+++ src/main/java/com/quicklist/app/controllers/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(originPatterns = {"http://localhost:4200/"})
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
